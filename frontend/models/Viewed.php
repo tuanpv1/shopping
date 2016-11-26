9ee84e2cd19e71bd73f156a07a1df3baad53dd79
tuanpv1 <tuantrylook@gmail.com>
2016-11-27 00:02:52 +0700

    fix viewed

--- frontend/models/Viewed.php
+++ frontend/models/Viewed.php
@@ -19,37 +19,33 @@ class Viewed
         }else{
             $viewed = $session['viewed'];
             foreach($viewed as $key => $value) {
-//                echo"<pre>";print($arrayData['id'].$value['id']);die();
-                if($value['id']== $arrayData['id'] && $key == 0){
-                    $viewed[0] = [
-                        'id'=>$arrayData['id'],
-                        'name'=> $arrayData['name'],
-                        'price'=> $arrayData['price'],
-                        'sale'=> $arrayData['sale'],
-                        'image'=> $arrayData['image'],
-                    ];
-                }elseif($value['id']== $arrayData['id']){
+                if($value['id']== $arrayData['id']){
                     unset($viewed[$key]);
                     $session['viewed'] = $viewed;
                 }
             }
             $session['viewed'] = $viewed;
-            $n = count($viewed);
-            $i = $n;
-//            echo"<pre>";print_r($viewed);die();
-            for($i=0 ; $i < $n; $i++ ){
-                $viewed[$i+1] = $viewed[$i];
-                if($viewed[$i]['id'] != $arrayData['id']){
-                    $viewed[$i] = [
-                        'id'=>$arrayData['id'],
-                        'name'=> $arrayData['name'],
-                        'price'=> $arrayData['price'],
-                        'sale'=> $arrayData['sale'],
-                        'image'=> $arrayData['image'],
-                    ];
-                }else{
-                    unset($viewed[$i]);
+            if($session['viewed'] == null){
+                $viewed[0] = [
+                    'id'=>$arrayData['id'],
+                    'name'=> $arrayData['name'],
+                    'price'=> $arrayData['price'],
+                    'sale'=> $arrayData['sale'],
+                    'image'=> $arrayData['image'],
+                ];
+            }else{
+                $n = count($viewed);
+//                echo"<pre>";print_r($n);die();
+                for($i = 1; $i <= $n; $i++){
+                    $viewed[$i] = $viewed[$i-1];
                 }
+                $viewed[0] = [
+                    'id'=>$arrayData['id'],
+                    'name'=> $arrayData['name'],
+                    'price'=> $arrayData['price'],
+                    'sale'=> $arrayData['sale'],
+                    'image'=> $arrayData['image'],
+                ];
             }
         }
         $session['viewed'] = $viewed;
