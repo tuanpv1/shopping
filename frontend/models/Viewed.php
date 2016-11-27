045aaca0c340aace9846098f84b6f7d6aef10b88
tuanpv1 <tuantrylook@gmail.com>
2016-11-27 12:17:54 +0700

    hope

--- frontend/models/Viewed.php
+++ frontend/models/Viewed.php
@@ -9,7 +9,7 @@ class Viewed
     public  function addViewed($id,$arrayData){
         $session = Yii::$app->session;
         if(!isset($session['viewed'])){
-            $viewed[0] = [
+            $viewed[] = [
                 'id'=>$arrayData['id'],
                 'name'=> $arrayData['name'],
                 'price'=> $arrayData['price'],
@@ -24,40 +24,15 @@ class Viewed
                     $session['viewed'] = $viewed;
                 }
             }
+            $viewed1 = [
+                'id'=>$arrayData['id'],
+                'name'=> $arrayData['name'],
+                'price'=> $arrayData['price'],
+                'sale'=> $arrayData['sale'],
+                'image'=> $arrayData['image'],
+            ];
+            array_unshift($viewed,$viewed1);
             $session['viewed'] = $viewed;
-            if($session['viewed'] == null){
-                $viewed[0] = [
-                    'id'=>$arrayData['id'],
-                    'name'=> $arrayData['name'],
-                    'price'=> $arrayData['price'],
-                    'sale'=> $arrayData['sale'],
-                    'image'=> $arrayData['image'],
-                ];
-            }else{
-//                echo"<pre>";print_r($viewed);die();
-                if(!isset($viewed[0])){
-                    $viewed[0] = [
-                        'id'=>$arrayData['id'],
-                        'name'=> $arrayData['name'],
-                        'price'=> $arrayData['price'],
-                        'sale'=> $arrayData['sale'],
-                        'image'=> $arrayData['image'],
-                    ];
-                }else{
-                    $n = count($viewed);
-//                    echo"<pre>";print_r($n);die();
-                    for($i = $n; $i >0; $i--){
-                        $viewed[$i] = $viewed[$i-1];
-                    }
-                    $viewed[0] = [
-                        'id'=>$arrayData['id'],
-                        'name'=> $arrayData['name'],
-                        'price'=> $arrayData['price'],
-                        'sale'=> $arrayData['sale'],
-                        'image'=> $arrayData['image'],
-                    ];
-                }
-            }
         }
 //        echo"<pre>";print_r($viewed);die();
         $session['viewed'] = $viewed;
