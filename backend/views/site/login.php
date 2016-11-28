763445746f8d20670183c8317fb0b2eb6993f2b8
tuanpv1 <tuantrylook@gmail.com>
2016-11-28 14:46:36 +0700

    fix

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -16,7 +16,6 @@ $this->params['breadcrumbs'][] = $this->title;
     <p>Please fill out the following fields to login:</p>
 
     <div class="row">
-        <div class="col-lg-5">
             <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
                 <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
@@ -29,7 +28,6 @@ $this->params['breadcrumbs'][] = $this->title;
                     <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
                 </div>
 
-            <?php ActiveForm::end(); ?>
-        </div>
+            <?php ActiveForm::end(); ?> 
     </div>
 </div>
