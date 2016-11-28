f00754b88357349d0a6efc3ac1998a0d367280f4
tuanpv1 <tuantrylook@gmail.com>
2016-11-28 14:48:41 +0700

    fix

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -7,13 +7,13 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
-$this->title = 'Login';
+$this->title = 'Đăng nhập';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out the following fields to login:</p>
+    <p>Vui lòng điền thông tin đăng nhập:</p>
 
     <div class="row">
             <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
@@ -28,6 +28,6 @@ $this->params['breadcrumbs'][] = $this->title;
                     <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
                 </div>
 
-            <?php ActiveForm::end(); ?> 
+            <?php ActiveForm::end(); ?>
     </div>
 </div>
